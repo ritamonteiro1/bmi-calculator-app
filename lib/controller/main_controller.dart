c0e0f451f10128100fa3e9684651d303a229dfc1
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-01-29 15:26:13 -0300

    create constants in main_controller.dart

--- lib/controller/main_controller.dart
+++ lib/controller/main_controller.dart
@@ -4,19 +4,28 @@ import 'package:flutter/cupertino.dart';
 
 class MainController {
   final bmi = ValueNotifier<Bmi>(Bmi.noInformationYet);
+  static const _maximumBmiIndexForUnderWeight = 18.6;
+  static const _maximumBmiIndexForIdealWeight = 24.9;
+  static const _maximumBmiIndexForSlightlyOverweight = 29.9;
+  static const _maximumBmiIndexForObesityLevelI = 34.9;
+  static const _maximumBmiIndexForObesityLevelII = 39.9;
 
   void calculateBMI(User user) {
     final bmiResult = user.weight / (user.height * user.height);
 
-    if (bmiResult < 18.6) {
+    if (bmiResult < _maximumBmiIndexForUnderWeight) {
       bmi.value = Bmi.underWeight;
-    } else if (bmiResult >= 18.6 && bmiResult < 24.9) {
+    } else if (bmiResult >= _maximumBmiIndexForUnderWeight &&
+        bmiResult < _maximumBmiIndexForIdealWeight) {
       bmi.value = Bmi.idealWeight;
-    } else if (bmiResult >= 18.6 && bmiResult < 24.9) {
+    } else if (bmiResult >= _maximumBmiIndexForIdealWeight &&
+        bmiResult < _maximumBmiIndexForSlightlyOverweight) {
       bmi.value = Bmi.slightlyOverweight;
-    } else if (bmiResult >= 24.9 && bmiResult < 34.9) {
+    } else if (bmiResult >= _maximumBmiIndexForSlightlyOverweight &&
+        bmiResult < _maximumBmiIndexForObesityLevelI) {
       bmi.value = Bmi.obesityLevelI;
-    } else if (bmiResult >= 34.9 && bmiResult < 39.9) {
+    } else if (bmiResult >= _maximumBmiIndexForObesityLevelI &&
+        bmiResult < _maximumBmiIndexForObesityLevelII) {
       bmi.value = Bmi.obesityLevelII;
     } else {
       bmi.value = Bmi.obesityLevelIII;
